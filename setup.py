8608b2743f6241883cae945334d3254a69b6a17e
jiaqiwang969 <jiaqiwang969@gmail.com>
2021-12-10 20:07:36 +0800

    forked by qiqiwang, the package can be installed

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+     name = 'numpad',
+     version = '0.1',
+     author = 'qiqiwang',
+     author_email = "qiqi@mit.edu",
+     url = "https://pypi.org/help/",
+     packages = ['numpad'],
+    )
+
+
+
+
+
+
